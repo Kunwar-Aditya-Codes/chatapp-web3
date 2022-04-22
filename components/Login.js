1c2460ff944e19c41951736e6abade8e2cda8e12
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-04-22 05:35:48 +0530

    background

---
+++ components/Login.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Login = () => {
+  return (
+    <div className="bg-black relative">
+      <div></div>
+      <div>
+        <Image
+          src="https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+          layout="fill"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
